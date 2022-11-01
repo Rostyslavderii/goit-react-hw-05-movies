52c6c8d9e153491e394162d0120c1a0d681fe873
Rostyslavderii <105818935+Rostyslavderii@users.noreply.github.com>
2022-11-01 18:08:09 +0200

    ggg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,8 @@ import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import { HomePage } from 'pages/Home';
-// import { MoviePage } from 'pages/MoviePage';
-// import { MovieDetails } from 'pages/MovieDetails';
+import { MoviePage } from 'pages/MoviePage';
+import { MovieDetails } from 'pages/MovieDetails';
 
 const MovieCast = lazy(() =>
   import('../pages/MoviesCast').then(module => ({
@@ -19,8 +19,8 @@ const MovieReview = lazy(() =>
   }))
 );
 
-const MovieDetails = lazy(() => import('../pages/MovieDetails'));
-const MoviePage = lazy(() => import('../pages/MoviePage'));
+// const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+// const MoviePage = lazy(() => import('../pages/MoviePage'));
 
 // const MovieDetails = lazy(() =>
 //   import('../pages/MovieDetails').then(module => ({
